f90df0bd615869f6891808da09ecf1364fd56372
Sandra Hufford <flourmill96@gmail.com>
2022-06-27 14:21:45 -0400

    fixed buttons to work as Tabs

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-<button type="button" @click="showClient = !showClient" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Client View</button>
-  <button type="button" @click="showOffice = !showOffice" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Office View</button>
-  <button type="button" @click="showSettings = !showSettings" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Settings</button>
+<button type="button" @click="activeTab = 'showClient'" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Client View</button>
+  <button type="button" @click="activeTab = 'showOffice'" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Office View</button>
+  <button type="button" @click="activeTab = 'showSettings'" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Settings</button>
  
  
-  <OfficeView v-if="showOffice" />
-  <ClientView v-if="showClient" />
-  <Settings v-if="showSettings" />
+  <OfficeView v-if= "activeTab === 'showOffice'" />
+  <ClientView v-if= "activeTab === 'showClient'" />
+  <Settings v-if= "activeTab === 'showSettings'" />
 
 
 </template>
@@ -17,8 +17,6 @@
   import Settings from "./components/Settings.vue"
   import {ref} from 'vue'
 
-  const showOffice = ref(false)
-  const showClient = ref(false)
-  const showSettings = ref(false)
+  const activeTab = ref('showOffice')
 
 </script>
